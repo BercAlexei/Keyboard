de13e31e326bb75a7eb879d5bd1fbfc69e2d4650
Alexei <1996Leha@mail.ru>
2021-04-09 21:15:32 +0500

    debug

--- src/js/_script.js
+++ src/js/_script.js
@@ -7,7 +7,8 @@ document.addEventListener('DOMContentLoaded', () => {
           sub = document.querySelector('#sub'),
           inps = document.querySelectorAll('input'),
           spans = document.querySelectorAll('span'),
-          inpsReq = document.querySelectorAll('[data-req]');
+          inpsReq = document.querySelectorAll('[data-req]'),
+          form = document.querySelector('form');
 
     function closeModal (){
         inps.forEach(item => {
@@ -42,27 +43,39 @@ document.addEventListener('DOMContentLoaded', () => {
             closeModal();
         } 
     })
-    
 
-    //Validation
+    //Validation код оставляет желать лучшего (но будем совершенствоваться)
     sub.addEventListener('click', (e) => {
         e.preventDefault();
-                inpsReq.forEach((item, i) => {
-                if (!item.validity.valid) {
-                    item.style.border = '1px solid red'
-                    spans.forEach((item, k) => {
-                        //! Чтобы красное сообщение об ошибке выдавалось только у определнного инпута нужно сделать условие строгого сравнения по номеру в массиве!!!!!!!!!!!!!!
-                        if (k === i) {
-                            item.style.display = 'block'
-                        }
-                    })
-                    
-                }
+        
+        let ar = [],
+            arTrue; 
 
-            }) 
+        inpsReq.forEach((item, key) => {
+            ar[key] = item;
+            if (!item.validity.valid) {
+                item.style.border = '1px solid red'
+                spans.forEach((red, k) => {
+                    //! Чтобы красное сообщение об ошибке выдавалось только у определнного инпута нужно сделать условие строгого сравнения по номеру в массиве!!!!!!!!!!!!!!
+                    if (k === key) {
+                        red.style.display = 'block'
+                    }
+                })
+            }
+            
         })
-    
-
+        arTrue = ar.every((item) => {
+            if (!item.validity.valid) {
+                
+            } else {
+                return true;
+            }
+        });
+        if(arTrue) {
+            closeModal()
+        }
+    })
+       
     inpsReq.forEach((item, i) => {
         item.addEventListener('input', () => {
             if (item.validity.valid) {
